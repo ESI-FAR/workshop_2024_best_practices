68120e164749fef0852b4084004d7659e61483a9
Ole Mussmann <gitlab+account@ole.mn>
2024-01-24 09:44:04 +0100

    remove useful newlines

--- 01_script.py
+++ 01_script.py
@@ -3,9 +3,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pypsa
-
 network = pypsa.Network()
-
 network.add("Bus", "My bus 0", v_nom=20.0)
 network.add("Bus", "My bus 1", v_nom=20.0)
 network.add("Bus", "My bus 2", v_nom=20.0)
@@ -20,22 +18,15 @@ network.add("Load", "Datacenter", bus="My bus 2", p_set=30, q_set=0)
 bla_0 = []
 bla_1 = []
 bla_2 = []
-
 for reactive_load in list(range(0, 1000, 50)):
     network.loads.loc["Factory", "q_set"] = reactive_load
-
     network.pf()
-
     bla_0.append(network.buses_t.v_ang[f"My bus 0"].iloc[0] / 180 * np.pi)
     bla_1.append(network.buses_t.v_ang[f"My bus 1"].iloc[0] / 180 * np.pi)
     bla_2.append(network.buses_t.v_ang[f"My bus 2"].iloc[0] / 18 * np.pi)
-
 fig, ax = plt.subplots()
-
 ax.plot(list(range(0, 1000, 50)), bla_0, label=f"Bus 0")
 ax.plot(list(range(0, 1000, 50)), bla_1, label=f"Bus 1")
 ax.plot(list(range(0, 1000, 50)), bla_2, label=f"Bus 2")
-
 ax.legend()
-
 plt.show()
